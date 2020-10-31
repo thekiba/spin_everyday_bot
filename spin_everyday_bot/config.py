66b5836676420f80aa121beb75e6ce8aaa5b0665
evgfilim1 <evgfilim1@yandex.ru>
2020-10-31 12:20:56 +0500

    Move to Pydantic settings management
    Add requirements.txt

--- spin_everyday_bot/config.py
+++ spin_everyday_bot/config.py
@@ -1,13 +1,6 @@
-from os import getenv
+from pydantic import BaseSettings
 
-# region Load config
-TOKEN = getenv("TOKEN", None)
-SUPERUSER = int(getenv("SUPERUSER", 0))
-# endregion
 
-# region Check config
-if TOKEN is None:
-    raise ImportError("TOKEN env var is not set!")
-if SUPERUSER == 0:
-    raise ImportError("SUPERUSER env var is not set!")
-# endregion
+class Settings(BaseSettings):
+    token: str
+    superuser: int
